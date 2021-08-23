401c676fdf10151e60c4d4b48fdc5eb6b8008123
Brieona <brieona.turner@gmail.com>
2021-08-23 18:17:55 -0400

    Adding files for the challenge

--- static/js/app.js
+++ static/js/app.js
@@ -21,46 +21,63 @@ function buildTable(data) {
       cell.text(val);
     });
   });
-}
+};
 
 // 1. Create a variable to keep track of all the filters as an object.
-
+var filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
+    let changedElement = d3.select(this);
+
 
     // 4b. Save the value that was changed as a variable.
+    let elementValue = changedElement.property("value");
+    console.log(elementValue);
+
 
     // 4c. Save the id of the filter that was changed as a variable.
+    let filtersId = changedElement.attr("id");
+    console.log(filtersId);
 
-  
+
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
+    if (elementValue) {
+      filters[filtersId] = elementValue;
+    }
+    else {
+      delete filters[filtersId];
+    }
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
-  }
+  };
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData ;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    for (const [filter_Id, value] of Object.entries(filters)) {
+      if (value) {
+        filteredData = filteredData.filter(row => row[filter_Id] === value);
+      }
+    }
+ 
   
     // 10. Finally, rebuild the table using the filtered data
-    
-  }
+    buildTable(filteredData)
+  };
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
